8e2e369f98b87da04f41983b277084abd0263e3a
Amir Hadzic <amir.hadzic@randomshouting.com>
2012-01-09 23:10:56 +0100

    Move file names to vars.

--- tools/build.sh
+++ tools/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-output="jscommits-min.js"
 src=".."
+output_js="$src/jscommits-min.js"
+output_css="$src/jscommits-min.css"
 
 cat $src/github.js $src/jscommits.js > temp.js
 
-rm $output 
-java -jar compiler.jar --js temp.js --js_output_file $src/$output
+rm $output_js
+java -jar compiler.jar --js temp.js --js_output_file $output_js
 rm temp.js
 
-cat $src/jscommits.css | ./cssmin.py > $src/jscommits-min.css
+cat $src/jscommits.css | ./cssmin.py > $output_css
